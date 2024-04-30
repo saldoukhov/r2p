ace39b209cc6b344ad07d082f5f53c74382bc1de
Sergey Aldoukhov <saldoukhov@gmail.com>
2024-04-29 22:22:18 -0700

    init

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'r2p';
+const urlsToCache = ['/', '/index.html'];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
+                }
+            )
+    );
+});
